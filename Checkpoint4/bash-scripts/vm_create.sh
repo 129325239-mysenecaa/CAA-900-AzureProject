85fb82485a216e77d8112c2a2f0bcaac6c92aa30
129325239-mysenecaa <icmajam@myseneca.ca>
2024-02-08 14:56:40 -0500

    up[date

--- Checkpoint4/bash-scripts/vm_create.sh
+++ Checkpoint4/bash-scripts/vm_create.sh
@@ -67,6 +67,7 @@ else
                 --data-disk-delete-option Delete \
                 --nics  $nic_name \
                 --ssh-key-values $public_sshkey_file \
+                --security-type Standard \
                 --no-wait
         #if [ $? ]; then echo "Returned Error! Aborting!"; exit 2; fi
     fi
